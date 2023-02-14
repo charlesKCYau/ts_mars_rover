f577a4f71f5923e5f549162832d45cac9ec10ddb
yaunga@hotmail.com <yaunga@hotmail.com>
2023-02-14 20:54:53 +0000

    update readme

--- README.md
+++ README.md
@@ -15,4 +15,7 @@ Performing the test case by typing in
 
 ## programs briefing
 There are 2 programs ts file and 1 testing ts file.
+
 main.ts is the main flow program and marsRover.ts is used to provide the Rover and the Plateau related data types and functions.
+
+There is a function createPlateau() to create the Plateau composing of array of coordinates. Currently it creates a rectangle or square with the input parameters. In future, it can be modified if it needs to create any shapes. And the subsequent logic are not necessarily needed to change.
--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { createPlateau, checkValidPos, Rover, examMovement, OrientalType, moveVe
 const fs = require("fs");
 const array: string[] = fs.readFileSync('input.txt', 'utf8').toString().split('\n');
 let coord : string[] = array[0].trim().split(" ");
-coord = coord.filter(a => a !== "");
+coord = coord.filter(a => a !== "");    //filter out the unnecessary item in case there are more than one spaces
 const plateau = createPlateau(Number.parseInt(coord[0]), Number.parseInt(coord[1]));
 
 for (let i=1; i<array.length; i=i+2) {
@@ -17,7 +17,7 @@ for (let i=1; i<array.length; i=i+2) {
     //Technically the below logic provide a flexibility to  handle with what the Plateau is in different shape
 
     let rover1 : string[] = array[i].trim().split(" ");
-    rover1 = rover1.filter(a => a !== "");
+    rover1 = rover1.filter(a => a !== "");    //filter out the unnecessary item in case there are more than one spaces
 
     if (checkValidPos(plateau, Number.parseInt(rover1[0]), Number.parseInt(rover1[1]))) {
 
